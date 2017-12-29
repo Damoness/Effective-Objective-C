871dce8411341da5a7bf8dc9bf76672a300a8a05
Damoness-Mini <qjwdcool@qq.com>
2017-12-29 09:06:29 +0800

    Item38:为常用的快类型创建typedef 1.以typedef重新定义快类型,可令快变量用起来更加简单 2.定义新类型时应遵从现在的命名习惯,勿使其名称与别的类型相冲突 3.不妨为同一个快签名定义多个类型别名.如果要重构的代码使用了快类型的某个别名,那么只需要修改相应typedef中的块签名即可,无需改动其他typedef.

---
+++ Item38 为常用的块类型创建typedef/Item38/Item38/ViewController.h
@@ -0,0 +1,25 @@
+//
+//  ViewController.h
+//  Item38
+//
+//  Created by Damoness on 2017/12/29.
+//  Copyright © 2017年 damoness. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+
+typedef int (^EOCSomeBlock)(BOOL flag,int value);
+
+typedef void (^completionHandler)(NSData *data,NSError *error);
+
+@interface ViewController : UIViewController
+
+
+
+-(void)startWithCompletionHandler:(completionHandler)handler;
+
+
+@end
+
---
+++ Item38 为常用的块类型创建typedef/Item38/Item38/ViewController.m
@@ -0,0 +1,42 @@
+//
+//  ViewController.m
+//  Item38
+//
+//  Created by Damoness on 2017/12/29.
+//  Copyright © 2017年 damoness. All rights reserved.
+//
+
+#import "ViewController.h"
+
+
+
+
+@interface ViewController ()
+
+@property (nonatomic,copy)EOCSomeBlock someBlock;
+
+@end
+
+@implementation ViewController
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+//    [self setSomeBlock:^int(BOOL flag, int value) {
+//
+//        return true;
+//    }];
+    
+
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
